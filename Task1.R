32eb34bfd8ba02b232c272e328c0ed00f3f4da78
hero.dot <kape.wolf@web.de>
2016-07-18 20:13:12 +0200

    files added

--- Task1.R
+++ Task1.R
@@ -1,14 +1,28 @@
 require(magrittr)
 require(dplyr)
 
-first <- scan("S01E01", what = "character", quiet = T, quote = "")
-first <- data.frame(first)
-first %>%
-  filter(!grepl("[0-9]+[:, ]", first))%>%
-  filter(!grepl("[0-9]+", first))%>%
-  filter(!grepl("-->",first))%>%
-  filter(!grepl("-",first))%>%
-  filter(!grepl("[A-Z]{2,}",first))%>%
-  mutate(first=gsub("\\?","",first))%>%
-  mutate(first=gsub("\\.","",first))%>%
-  mutate(first=gsub("\\,","",first))-> tidy_text
+allText <- NULL
+for (i in 1:6) {
+  season <- paste0("S0",i)
+  for (j in 1:10) {
+    episode <- ifelse(j<10,paste0("E0",j), paste0("E",j))
+    seaEp <- paste0(season, episode)
+    
+    file <- scan(seaEp, what = "character", quiet = T, quote = "")
+    words <- data.frame(file)
+    colnames(words) <- c("word")
+    words %>%
+      filter(!grepl("[0-9]+[:, ]", word))%>%
+      filter(!grepl("[0-9]+", word))%>%
+      filter(!grepl("-->",word))%>%
+      filter(!grepl("-",word))%>%
+      filter(!grepl("[A-Z]{2,}",word))%>%
+      mutate(word=gsub("\\?","",word))%>%
+      mutate(word=gsub("\\.","",word))%>%
+      mutate(word=gsub("\\,","",word))-> tidyText
+    
+    tidyText <- cbind(tidyText, season = rep(season,nrow(tidyText)), episode = rep(episode,nrow(tidyText)))
+    
+    allText <- rbind(allText, tidyText)
+  }
+}
